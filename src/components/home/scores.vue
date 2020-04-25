45e8b4a50ad9ae25736966678a1f098046a42839
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2020-04-25 18:59:46 +0530

    save and show scores

--- src/components/home/scores.vue
+++ src/components/home/scores.vue
@@ -2,14 +2,40 @@
   <section>
     <ui-container>
       <ui-h1>Scores</ui-h1>
+      <ui-card v-if="solves.length > 0">
+        <b>{{ solveHeader }}</b><br>
+        <b>Time:</b> {{ solves[showSolve].timestring }}<br>
+        <b>Scramble:</b> {{ solves[showSolve].scramble }}
+      </ui-card>
+      <table style="table-layout: auto; width: 100%; text-align: center;">
+        <thead>
+          <th>#</th>
+          <th>time</th>
+          <th>ao5</th>
+          <th>ao12</th>
+        </thead>
+        <tr
+          v-for="(solve, index) in solves"
+          :key=index
+          @click="showSolve = index"
+          style="cursor: pointer;"
+        >
+          <td>{{ getSolveNumber(index) }}</td>
+          <td>{{ solve.timestring }}</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+      </table>
     </ui-container>
   </section>
 </template>
 
 <script>
+import { getTimestring } from '../../utils/common';
 import {
   UiContainer,
   UiH1,
+  UiCard,
 } from '../../ui';
 
 export default {
@@ -18,6 +44,59 @@ export default {
   components: {
     UiContainer,
     UiH1,
+    UiCard,
+  },
+
+  data() {
+    return {
+      solves: [],
+      showSolve: 0,
+    };
+  },
+
+  mounted() {
+    this.loadFromStorage();
+    this.$bus.$on('timer-stop', this.addSolve);
+  },
+
+  computed: {
+    solveHeader() {
+      return `Solve #${this.getSolveNumber(this.showSolve)}`;
+    },
+  },
+
+  methods: {
+    addSolve() {
+      const { solveTime } = this.$store.state;
+      const solve = {
+        time: solveTime,
+        timestring: getTimestring(solveTime),
+        scramble: this.$store.state.currentSolvedScramble,
+      };
+
+      this.solves.unshift(solve);
+      this.saveToStorage();
+    },
+
+    saveToStorage() {
+      localStorage.setItem('solves', JSON.stringify(this.solves));
+    },
+
+    loadFromStorage() {
+      let solves = [];
+      try {
+        solves = JSON.parse(localStorage.getItem('solves') || '[]');
+      } catch (e) {
+        // Remove corrupted item from storage
+        localStorage.removeItem('solves');
+      }
+
+      this.solves = solves;
+    },
+
+    getSolveNumber(index) {
+      return this.solves.length - index;
+    },
   },
 };
 </script>
