29a8ab0cc0c5f72e300fde2f2fe394e3377535cb
Lakshya Gupta <theultimateaim@gmail.com>
2020-05-02 14:24:48 +0530

    Add data file

--- src/components/home/oll.vue
+++ src/components/home/oll.vue
@@ -2,22 +2,49 @@
   <section>
     <ui-container>
       <ui-h1>OLL</ui-h1>
+
+      <oll-unit
+        v-for="oll in this.olls"
+        :key="oll.name"
+        :tf="oll.tf"
+        :sf="oll.sf"
+        :text="oll.algorithms[0]"
+        class="oll-unit"
+      />
     </ui-container>
   </section>
 </template>
 
 <script>
+import data from '../../data';
+
 import {
   UiContainer,
   UiH1,
 } from '../../ui';
 
+import OllUnit from './oll-unit.vue';
+
 export default {
   name: 'Oll',
 
   components: {
+    OllUnit,
     UiContainer,
     UiH1,
   },
+
+  data: () => ({
+    olls: data.oll,
+  }),
 };
 </script>
+
+<style scoped>
+.oll-unit {
+  margin-bottom: 12px;
+}
+.oll-unit:last-child {
+  margin-bottom: 0;
+}
+</style>
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default {
+  oll: [
+    {
+      name: 'OCLL6 - OLL 26',
+      tf: ['TM', 'TR', 'ML', 'MM', 'MR', 'BM'],
+      sf: ['BL', 'LT', 'RB'],
+      chance: 54,
+      algorithms: [
+        ["RU2R'U'RU'R'"],
+        ["y'R'U'RU'R'U2R"],
+      ],
+    },
+
+    {
+      name: 'OCLL7 - OLL 27',
+      tf: ['TM', 'ML', 'MM', 'MR', 'BL', 'BM'],
+      sf: ['TL', 'RT', 'BR'],
+      chance: 54,
+      algorithms: [
+        ["R U R' U R U2' R'"],
+        ["y' R' U2' R U R' U R"],
+      ],
+    },
+
+    {
+      name: 'OCLL1 - OLL 21',
+      tf: ['TM', 'ML', 'MM', 'MR', 'BM'],
+      sf: ['TL', 'TR', 'BL', 'BR'],
+      chance: 108,
+      algorithms: [
+        ["R U2 R'", "U' R U R'", "U' R U' R'"],
+        ['y', "R U R' U", "R U' R' U", "R U2' R'"],
+      ],
+    },
+
+
+    {
+      name: 'OCLL2 - OLL 22',
+      tf: ['TM', 'ML', 'MM', 'MR', 'BM'],
+      sf: ['LT', 'LB', 'TR', 'BR'],
+      chance: 54,
+      algorithms: [
+        ["R U2' R2' U' R2 U' R2' U2' R "],
+      ],
+    },
+  ],
+};
