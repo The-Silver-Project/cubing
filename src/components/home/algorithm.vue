897bb43a7ca444eee2e75e03987bfaa977554bfc
Lakshya Gupta <theultimateaim@gmail.com>
2020-05-09 21:53:50 +0530

    Add color codes for triggers to algorithms

--- src/components/home/algorithm.vue
+++ src/components/home/algorithm.vue
@@ -3,7 +3,7 @@
     <div
       v-for="(unit, index) in text"
       :key="index"
-      :class="unit.replace('\'', '-')"
+      :class="unit.replace(/'/g, '-')"
     >{{ unit }}</div>
   </ui-columns>
 </template>
@@ -26,10 +26,18 @@ export default {
 
 <style scoped>
 div {
+  margin-right: 9px;
+  font-size: 12px;
   font-weight: 700;
-  margin-right: 6px;
+  color: rgb(94, 94, 94);
+  background-color: rgba(141, 141, 141, 0.11)
 }
-div:last-child {
-  margin-right: 0;
+.RUR-U- {
+  color: rgb(253, 56, 148);
+  background-color: rgba(253, 56, 148, 0.137);
+}
+.R-FRF- {
+  color: rgb(59, 185, 0);
+  background-color: rgba(59, 185, 0, 0.13);
 }
 </style>
